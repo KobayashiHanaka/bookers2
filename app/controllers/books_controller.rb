4b31bac1368759ec1ffba63cefcad8bd74210a53
KobayashiHanaka <konahana8787@gmail.com>
2022-05-28 10:32:36 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+
+  def show
+    @book=Book.new
+    @book2=Book.find(params[:id])
+    @user=@book2.user
+  end
+
+  def index
+    @book=Book.new
+    @books=Book.all
+    @user=current_user
+  end
+
+
+
+  def edit
+    @book=Book.find(params[:id])
+    unless @book.user==current_user
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
+  end
+
+
+  def create
+    @user=current_user
+    @books=Book.all
+
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body,:user_id)
+  end
+
+end
